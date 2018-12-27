d0fb9ea89010bb6b12ca91d0c120e1ddfd679ae1
blukasho <bodik1w@gmail.com>
2018-12-27 21:45:59 +0200

    reverse_bits.c

--- 02/main.c
+++ 02/main.c
@@ -6,22 +6,32 @@
 /*   By: blukasho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 19:36:17 by blukasho          #+#    #+#             */
-/*   Updated: 2018/12/20 19:47:56 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/12/27 16:46:59 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int		is_power_of_2(unsigned int n);
-int		ft_is_power_of_2(unsigned int n);
+void				print_bits(unsigned char octet);
+//unsigned char		reverse_bits(unsigned char octet);
 
-int		main(int argc, char **argv)
+int					main(int argc, char **argv)
 {
-	int	n;
-
-	n = -1;
-	while (++n < 1000)
-		printf("result %d %d %d\n", n, is_power_of_2(n), ft_is_power_of_2(n));
+	unsigned char	n = atoi(argv[1]);
+	unsigned char	r = 0;
+	int				i = 9;
+	print_bits(n);
+	write(1, "\n", 1);
+	while (--i > 0)
+	{
+		r <<= 1;
+		if (n & 1 == 1)
+			r ^= 1;
+		n >>= 1;
+		print_bits(r);
+		write(1, "\n", 1);
+	}
 	return (printf("\n"));
 }
